f8a2b9aa9dfbb93ce7263a6f8ca3c1e07eee313c
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-06-27 17:48:06 +0200

    Milestone 2

--- index.js
+++ index.js
@@ -7,12 +7,28 @@ let reset = document.getElementById("reset");
 let clock;
 let check = true;
 
-let counter = 0.00;
+let centesimi = "00";
+let seconds = "00";
+let minutes = "00";
+let ore = "00";
+
+output.innerHTML = `${ore} : ${minutes} : ${seconds} : ${centesimi}`;
 start.addEventListener("click", function(){
     if(check){
         clock = setInterval(function(){
-            counter = (parseFloat(counter) + 0.01).toFixed(2);
-            output.innerHTML = counter;
+            centesimi = (parseFloat(centesimi) + 1);
+            if(centesimi === 100){
+                centesimi = 0;
+                seconds++;
+            }else if(seconds === 60){
+                seconds = 0;
+                minutes++;
+            }else if(minutes === 60){
+                minutes = 0;
+                ore++;
+            }
+
+            output.innerHTML = `${ore} : ${minutes} : ${seconds} : ${centesimi}`;
         }, 10);
         check = false;
     }
@@ -25,7 +41,10 @@ stop.addEventListener("click", function(){
 
 reset.addEventListener("click", function(){
     clearInterval(clock);
-    counter = 0.00;
-    output.innerHTML = counter;
+    centesimi = 0;
+    seconds = 0;
+    minutes = 0;
+    ore = 0;
+    output.innerHTML = `${ore} : ${minutes} : ${seconds} : ${centesimi}`;
     check = true;
 });
